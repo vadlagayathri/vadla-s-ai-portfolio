1206e9d41bbb7c6bb48b32107763b95537dc487b
vadlagayathri <gayathrivadla4@gmail.com>
2026-01-28 14:28:46 +0530

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/vadla-s-ai-portfolio/",   // 👈 REQUIRED FOR GITHUB PAGES
   server: {
     host: "::",
     port: 8080,
